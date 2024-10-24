8f58f6ad92fead63d5e3436ce03f8b8e3229ca3a
hxrshxz <harshmastic@gmail.com>
2024-10-24 20:10:50 +0530

    fixed bugs

--- script.js
+++ script.js
@@ -19,77 +19,38 @@ form.addEventListener("submit", (e) => {
     const includeUppercase = form.includeUppercase.checked;
     const includeNumbers = form.includeNumbers.checked;
     const includeSymbols = form.includeSymbols.checked;
-    let verifiedPassword = false;
 
 
-    while (verifiedPassword === false) {
-        let password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
-        checkPassword(password);
-        if(checkPassword(password) === true) {
-            verifiedPassword = true;
-            document.getElementById('password').innerText = password
-        }
-    }
+    document.getElementById('password').innerText = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols);
+
+
 })
 
 function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols) {
 
 
     let characters = "abcdefghijklmnopqrstuvwxyz";
-
-
-    if (includeUppercase) {
-        characters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    }
-    if (includeNumbers) {
-        characters += "0123456789";
-    }
-    if (includeSymbols) {
-        characters += "!@#$%^&*(){}[]=<>/,.";
-    }
     let password = "";
-    for (let i = 0; i < characterAmount; i++) {
-        password += characters.charAt(Math.floor(Math.random() * characters.length));
-    }
-    return password;
-
-
-}
-
-function checkPassword(password) {
     let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     let numbers = "0123456789";
     let symbols = "!@#$%^&*(){}[]=<>/,.";
-    var upperCaseIncluded = false;
-    var numbersIncluded = false;
-    var symbolsIncluded = false;
-    for (let i = 0; i < password.length; i++) {
-        for (let j = 0; j < upperCase.length; j++) {
-            if (password.charAt(i) === upperCase[j]) {
-                upperCaseIncluded = true;
-            }
-        }
-        for (let j = 0; j < numbers.length; j++) {
-            if (password.charAt(i) === numbers[j]) {
-                numbersIncluded = true;
-            }
 
-        }
-        for (let j = 0; j < symbols.length; j++) {
-            if (password.charAt(i) === symbols[j]) {
-                symbolsIncluded = true;
-            }
 
+    for (let i = 0; i < characterAmount; i++) {
+        if (password.length < characterAmount) {
+            password += characters.charAt(Math.floor(Math.random() * characters.length));
+        }
+        if (includeUppercase && password.length < characterAmount) {
+            password += upperCase[Math.floor(Math.random() * upperCase.length)];
+        }
+        if (includeNumbers && password.length < characterAmount) {
+            password += numbers[Math.floor(Math.random() * numbers.length)];
+        }
+        if (includeSymbols && password.length < characterAmount) {
+            password += symbols[Math.floor(Math.random() * symbols.length)];
         }
     }
-    if ((upperCaseIncluded && form.includeUppercase.checked)|| numbersIncluded && form.includeNumbers.checked || symbolsIncluded && form.includeSymbols.checked ) {
-        return true;
 
-    } else if(!form.includeUppercase.checked && !form.includeNumbers.checked && !form.includeSymbols.checked) {
-        return true;
-    } else {
-        return false;
-    }
+    return password;
 }
-
 
